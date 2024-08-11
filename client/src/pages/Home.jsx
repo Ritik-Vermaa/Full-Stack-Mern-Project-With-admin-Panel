203b62dabe5c05f3b1e51cc7bbf4a8b75f928d44
Ritik verma <vermaritik933@gmail.com>
2024-08-11 21:21:22 +0530

    Making Home Section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,80 @@
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
+  return (
+    <>
+      <main>
+        <section className="section-hero">
+          <div className="container grid grid-two-cols">
+            <div className="hero-content">
+              <p>we are the world best it company</p>
+              <h1>welcome to Ritik Verma</h1>
+              <p>
+                Are you Ready to take your business to the next level with
+                cutting edge It solutions? Look no further! We are here to help
+                you with all your IT needs.
+              </p>
+              <div className="btn btn-group">
+                <a href="/contact">
+                  <button className="btn">Connect Now</button>
+                </a>
+                <a href="/service">
+                  <button className="btn secondary-btn">Learn More</button>
+                </a>
+              </div>
+            </div>
+            <div className="hero-image">
+              <img src="/images/home.png" alt="" height={400} width={400} />
+            </div>
+          </div>
+        </section>
+      </main>
+
+      {/* 2nd section  */}
+
+      <section className="section-analytics">
+        <div className="container grid grid-four-cols">
+            <div className="div1">
+                <h2>50+</h2>
+                <p>registerd</p>
+            </div>
         </div>
-    );
-}
+
+      </section>
+
+      {/* 3rd section  */}
+      <section className="section-hero">
+        <div className="container grid grid-two-cols">
+          {/* hero images  */}
+          <div className="hero-image">
+            <img
+              src="/images/design.png"
+              alt="coding together"
+              width="400"
+              height="500"
+            />
+          </div>
+
+          <div className="hero-content">
+            <p>We are here to help you</p>
+            <h1>Get Started Today</h1>
+            <p>
+              Ready to take the first step towards a more efficient and secure
+              IT infrastructure? Contact us today for a free consultation and
+              let's discuss how Thapa Technical can help your business thrive in
+              the digital age.
+            </p>
+            <div className="btn btn-group">
+              <a href="/contact">
+                <button className="btn">connect now</button>
+              </a>
+              <a href="/services">
+                <button className="btn secondary-btn">learn more</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Home;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,71 @@
+import { useState } from "react";
+
 const Login = () => {
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUser({ ...user, [name]: value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <section>
+        <main>
+          <div className="section-login">
+            <div className="container grid grid-two-cols">
+              <div className="login-image">
+                <img src="/images/login.png" alt="" 
+                width={400} 
+                height={400} />
+              </div>
+              <div className="login-form">
+              <h1 className="main-heading mb-3">Login Form</h1>
+              <br />
+              <form onSubmit={handleSubmit}>
+              <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-submit" >Login Now</button>
+              </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
   );
 };
 
