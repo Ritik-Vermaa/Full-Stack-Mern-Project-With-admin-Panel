2d79209594cd5c6419e33fb66b3720d32ee53ba0
Ritik verma <vermaritik933@gmail.com>
2024-08-11 15:18:56 +0530

    Registration Page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,26 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
 export const Navbar = () => {
     return (
         <>
         <header>
             <div className="container">
                 <div className="logo-brand">
-                    <a href="/">Ritik Verma</a>
+                    <NavLink to="/">Ritik Verma</NavLink>
                 </div>
-            </div>
-            <nav>
+                <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                    <li><a href="/service">Service</a></li>
-                    <li><a href="/register">Register</a></li>
-                    <li><a href="/login">Login</a></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/service">Service</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                    <li><NavLink to="/register">Register</NavLink></li>
+                    <li><NavLink to="/login">Login</NavLink></li>
                 </ul>
             </nav>
+            </div>
+            
         </header>
         </>
     );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,110 @@
+import { useState } from "react";
+
 const Register = () => {
+
+  const [user , setUser] = useState({
+    username : "",
+    email : "",
+    phone : "",
+    password : "",
+
+  });
+
+  //Handling the input values
+  const handleInput = (e) =>{
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser(
+      {
+        ...user,
+        [name] : value,
+      }
+    );
+  }
+
+  //Handling the form submission
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+  }
+
   return (
     <>
-      <h1>Register</h1>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/register.png"
+                  alt=""
+                  width="400"
+                  height="400"
+                />
+              </div>
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">Registration Form</h1>
+                <br />
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="username">username</label>
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="username"
+                      id="username"
+                      required
+                      autoComplete="off"
+                      value={user.username}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone">phone</label>
+                    <input
+                      type="number"
+                      name="phone"
+                      placeholder="phone"
+                      id="phone"
+                      required
+                      autoComplete="off"
+                      value={user.phone}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-submit" >Register Now</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
     </>
   );
 };
