080dbb50ec8d97bfd5b0f368748a2dc191bce637
Ritik verma <vermaritik933@gmail.com>
2024-08-18 14:42:31 +0530

    Creating About Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import Analytics from "../components/Analytics";
+
 const Home = () => {
   return (
     <>
@@ -29,27 +31,8 @@ const Home = () => {
       </main>
 
       {/* 2nd section  */}
-
-      <section className="section-analytics">
-        <div className="container grid grid-four-cols">
-          <div className="div1">
-            <h2>50+</h2>
-            <p>registerd companies</p>
-          </div>
-          <div className="div1">
-            <h2>100,00+</h2>
-            <p>Happy Clients</p>
-          </div>
-          <div className="div1">
-            <h2>500+</h2>
-            <p>well known Developer</p>
-          </div>
-          <div className="div1">
-            <h2>24/7</h2>
-            <p>services</p>
-          </div>
-        </div>
-      </section>
+      <Analytics/>
+      
 
       {/* 3rd section  */}
       <section className="section-hero">
